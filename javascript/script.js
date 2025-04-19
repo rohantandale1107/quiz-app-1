8e3cf1572690fbeaf76258c05252f37e9d78d7cd
rohantandale1107 <rohanhtandale11@gmail.com>
2025-04-19 18:47:10 +0530

    commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,29 @@
-let quizCategory= 'programming'
+const answerOptions= document.querySelector(".answer-options");
+let quizCategory= "programming";
+
 
 const getRandomQuestion = ()=>{
-    const categoryQuestions= questions.find(cat=>cat.category.toLowerCase()===quizCategory.toLowerCase());
-    console.log(categoryQuestions);
+    const categoryQuestions= questions.find(cat=>cat.category.toLowerCase()===quizCategory.toLowerCase()).questions || [];
+
+    const randomQuestion = categoryQuestions[Math.floor(Math.random()*categoryQuestions.length)];
+    return randomQuestion;
     
 }
-getRandomQuestion();
+
+const renderQuestion =()=>{
+    const currentQuestion= getRandomQuestion();
+    if(!currentQuestion) return;
+    console.log(currentQuestion);
+    
+    answerOptions.innerHTML="";
+    document.querySelector(".question-text").textContent= currentQuestion.question;
+
+    currentQuestion.options.forEach(option=>{
+        const li= document.createElement("li");
+        li.classList.add("answer-option");
+        li.textContent= option;
+        answerOptions.appendChild(li);
+    })
+}
+renderQuestion();
+
